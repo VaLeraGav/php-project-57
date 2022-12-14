6795c677b77f9e55367da80301523f0ab4c6bef8
Valeriy <95502737+VaLeraGav@users.noreply.github.com>
2022-12-14 11:01:28 +0300

    did not see expected text

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'Enter the correct username and password.',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 

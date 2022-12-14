da2c8353e71ac6084ef142f49db5589a99159c14
Valeriy <95502737+VaLeraGav@users.noreply.github.com>
2022-12-14 11:10:36 +0300

    did not see expected text

--- lang/en/flash.php
+++ lang/en/flash.php
@@ -2,20 +2,20 @@
 
 return [
     'label' => [
-        'added' => 'Label added successfully!',
-        'edited' => 'Label edited successfully!',
-        'deleted' => 'Label deleted successfully!',
-        'failed' => 'Failed to delete label!'
+        'added' => 'Label added successfully',
+        'edited' => 'Label edited successfully',
+        'deleted' => 'Label deleted successfully',
+        'failed' => 'Failed to delete label'
     ],
     'status' => [
-        'added' => 'Status added successfully!',
-        'edited' => 'Status edited successfully!',
-        'deleted' => 'Status deleted successfully!',
-        'failed' => 'Failed to delete status!'
+        'added' => 'Status successfully created',
+        'edited' => 'Status edited successfully',
+        'deleted' => 'Status deleted successfully',
+        'failed' => 'Failed to delete status'
     ],
     'task' => [
-        'added' => 'Task added successfully!',
-        'edited' => 'Task edited successfully!',
-        'deleted' => 'Task deleted successfully!',
+        'added' => 'Task added successfully',
+        'edited' => 'Task edited successfully',
+        'deleted' => 'Task deleted successfully',
     ],
 ];

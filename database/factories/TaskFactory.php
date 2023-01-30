60664b25f8a1f7691d909eb207cba6c3d72f590c
Valeriy <95502737+VaLeraGav@users.noreply.github.com>
2023-01-30 18:01:31 +0300

    test(factory): add random User

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use App\Models\Task;
+use App\Models\TaskStatus;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class TaskFactory extends Factory
@@ -21,13 +23,20 @@ class TaskFactory extends Factory
      */
     public function definition()
     {
-        // переделать на рандомные числа
+        $randomIdStatus = random_int(1, count(TaskStatus::get()));
+
+        $maxIdUser = count(User::all());
+
+        $randomIdAssigned = User::find(random_int(1, $maxIdUser));
+
+        $randomIdCreater = User::find(random_int(1, $maxIdUser));
+
         return [
-            'status_id' => 1,
+            'status_id' => $randomIdStatus,
             'name' => $this->faker->unique()->name(),
             'description' => $this->faker->unique()->text(100),
-            'created_by_id' => 1,
-            'assigned_to_id' => 1,
+            'created_by_id' => $randomIdCreater,
+            'assigned_to_id' => $randomIdAssigned,
         ];
     }
 }
